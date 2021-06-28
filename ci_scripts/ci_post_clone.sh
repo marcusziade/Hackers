9f900e84cf1027c47f85a2e20a8f377e6729d94d
Weiran Zhang <weiran@zhang.me.uk>
2021-06-28 21:05:59 +0100

    Back to homebrew

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -6,7 +6,6 @@
 #  Created by Weiran Zhang on 28/06/2021.
 #  Copyright © 2021 Glass Umbrella. All rights reserved.
 
-#brew cleanup -d -v
-#brew install cocoapods
-gem install cocoapods
+brew install cocoapods
+
 pod install
