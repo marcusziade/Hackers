776ba9701deb61a4a70a194d56b9c3437653f0ce
Weiran Zhang <weiran@zhang.me.uk>
2021-06-28 20:22:41 +0100

    Add Xcode Cloud script

---
+++ ci_scripts/ci_post_clone.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+#  ci_post_clone.sh
+#  Hackers
+#
+#  Created by Weiran Zhang on 28/06/2021.
+#  Copyright © 2021 Glass Umbrella. All rights reserved.
+
+brew install cocoapods
+pod install
